1979cff0a4cadd581f40005ea40eb4e6828d2d18
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-08-25 00:31:37 -0400

    se llaman servicios mediante Eureka

--- movie-catalog-service/src/main/java/io/kadevl/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/kadevl/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,6 +2,7 @@ package io.kadevl.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -12,12 +13,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class MovieCatalogServiceApplication {
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
-	}
-	
-	public WebClient.Builder getWebClientBuilder(){
-		return WebClient.builder();
 	}
 	
 	public static void main(String[] args) {
--- movie-catalog-service/src/main/java/io/kadevl/moviecatalogservice/controller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/io/kadevl/moviecatalogservice/controller/MovieCatalogController.java
@@ -1,21 +1,16 @@
 package io.kadevl.moviecatalogservice.controller;
 
-import java.lang.reflect.ParameterizedType;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import io.kadevl.moviecatalogservice.model.CatalogItem;
 import io.kadevl.moviecatalogservice.model.Movie;
-import io.kadevl.moviecatalogservice.model.Rating;
 import io.kadevl.moviecatalogservice.model.UserRating;
 
 @RestController
@@ -25,18 +20,13 @@ public class MovieCatalogController {
 	@Autowired
 	private RestTemplate restTemplate;
 
-	@Autowired
-	private WebClient.Builder webClientBuilder;
-
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-		UserRating ratings = restTemplate.getForObject("http://localhost:8082/ratings/users/ " + userId, UserRating.class);
+		UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratings/users/ " + userId, UserRating.class);
 
 		return ratings.getUserRating().stream().map(rating -> {
-			// Por cada ID de pelicula, obtengo la información de aquella.
-			Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
-			// Retorno todo junto.
+			Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
 			return new CatalogItem(movie.getName(), "Description - " + movie.getName(), rating.getRating());
 		}).collect(Collectors.toList());
 	}
