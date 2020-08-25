dcb982122ee3e8075a70cd43f17d8e137746faec
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-08-24 23:02:23 -0400

    consumiendo servicio movie-info-service en movie-catalog-service

--- movie-catalog-service/src/main/java/io/kadevl/moviecatalogservice/controller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/io/kadevl/moviecatalogservice/controller/MovieCatalogController.java
@@ -1,21 +1,32 @@
 package io.kadevl.moviecatalogservice.controller;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import io.kadevl.moviecatalogservice.model.CatalogItem;
+import io.kadevl.moviecatalogservice.model.Movie;
+import io.kadevl.moviecatalogservice.model.Rating;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogController {
 
 	@RequestMapping("/{userId}")
-	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-		return Collections.singletonList(
-				new CatalogItem("Forrest Gump", "Forrest Gump, Tom Hanks", 10));
+	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		List<Rating> ratings = Arrays.asList(new Rating("M01", 10), new Rating("M02", 6));
+
+		return ratings.stream().map(rating -> {
+			Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
+			return new CatalogItem(movie.getName(), "description - " + movie.getName(), rating.getRating());
+		}).collect(Collectors.toList());
 	}
 }
